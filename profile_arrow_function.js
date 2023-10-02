977e79fa677ee25d84d742302a81b4ce69bfaee5
reezyee <rezasapitra514@gmail.com>
2023-10-02 12:46:49 +0700

    init

---
+++ profile_arrow_function.js
@@ -0,0 +1,6 @@
+const profile = (namaDepan, namaBelakang, email) => {
+    console.log("Nama : " + namaDepan +" "+ namaBelakang) 
+    console.log("Email : " + email)
+}
+
+profile("Reza","Sapitra","@rezasapitra514")
